bf2ab88274f9582a932c7a61f69a3475b010e605
Artem Isachenkov <artemyt237@gmail.com>
2024-06-03 20:33:59 +0300

    added Person added Flight

---
+++ go.mod
@@ -0,0 +1,7 @@
+module aviasales
+
+go 1.22.3
+
+require github.com/RashRacess/Flight v0.3.3
+
+require github.com/RashRacess/Person v0.1.1
---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	fl "github.com/RashRacess/Flight"
+	p "github.com/RashRacess/Person"
+)
+
+func main() {
+	flight := fl.Flight{}
+	person := p.Person{}
+
+	fmt.Println(flight)
+	fmt.Println(person)
+
+}
